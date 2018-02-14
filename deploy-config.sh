8ff1d16c79bc7391bf381ee406858fb00c2cf727
kevin crawley <kcrawley@franklinamerican.com>
2018-02-14 13:57:22 -0600

    update config deployment

--- deploy-config.sh
+++ deploy-config.sh
@@ -11,26 +11,20 @@ if [ -z "${BASE_DIR}" ]; then
     export BASE_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 fi
 
-configList=$(find ${BASE_DIR}/config -type f)
-
-echo $configList
-
-exit
-
-for file in ${WORK_DIR}/${CONFIG_PATH}/*; do
-    foPrintLine "processing: ${file}"
+for file in ${BASE_DIR}/config/*; do
+    echo "processing: ${file}"
     if [ -f ${file} ]; then
         fileName=${file##*/}
         baseName=${fileName%.*}
 
         if [ ! -z $(docker config ls -q --filter name=${baseName}) ]; then
-            foPrintLine "removing: ${baseName}" "yellow"
+            echo "attempting to remove: ${baseName}"
 
-            result=$(docker config rm ${baseName} 2>&1) || handleError "delete" ${result} ${baseName}
+            docker config rm ${baseName} 2>&1 || true
         fi
 
-        foPrintLine "creating: ${baseName} [${CONFIG_PATH}/${fileName}]" "green"
+        echo "attempting to create: ${baseName} [${BASE_DIR}/${fileName}]"
 
-        result=$(docker config create $LABELS_JOINED $baseName $file 2>&1) || handleError "create" ${result} ${baseName}
+        docker config create $baseName $file || true
     fi;
 done
